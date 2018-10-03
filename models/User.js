a5246dee28d70ce1864df698f0db131d59952b9f
Juan Jose <juanjomh09@gmail.com>
2018-10-02 21:08:13 -0600

    Test

---
+++ models/User.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    name : { type: String, required: true },
+    email : { type: String, required: true },
+    password : { type: String, required: true },
+    avatar : { type: String},
+    date : { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('users', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const router = express.Router();
 
-// @route      GET api/profile/test
+// @route      GET api/profile  /test
 // @desc       Tests profile route
 // @access     Public
 router.get('/test', (req, res) => res.json({msg: 'Profile works'}));
